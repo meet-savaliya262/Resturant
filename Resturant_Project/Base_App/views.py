fd1b226d7edb10fc5b3776d41497eb4257cb3c62
meet-savaliya262 <meetsavliya13@gmail.com>
2025-09-20 18:43:33 +0530

    Initialization

---
+++ Resturant_Project/Base_App/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def HomeView(request):
+    pass
+
+def HomeView(request):
+    pass
+
+
+def HomeView(request):
+    pass
+
+
+def HomeView(request):
+    pass
